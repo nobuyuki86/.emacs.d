514a1272d6a8457cd4062dc5c3ded92a60773014
Kamimoto Nobuyuki <nobuyuki86@outlook.jp>
2022-05-13 00:21:45 +0900

    Added org-modern

--- init.el
+++ init.el
@@ -512,6 +512,14 @@
 
 (add-hook 'sql-mode-hook 'sqlind-minor-mode)
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; #org
+
+(straight-use-package 'org-modern)
+
+(add-hook 'org-mode-hook 'org-modern-mode)
+
 (provide 'init)
 ;;; init.el ends here
 
