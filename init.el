98e86054e7a36f8192b02b4b9adff0c26808b4e7
Kamimoto Nobuyuki <nobuyuki86@outlook.jp>
2022-05-16 22:53:25 +0900

    Added company-dabbrev settings.
    Added settings to improve speed.

--- init.el
+++ init.el
@@ -97,6 +97,8 @@
 (with-eval-after-load 'company
   (setq company-idle-delay 0
 	company-minimum-prefix-length 1
+	company-dabbrev-ignore-case nil
+	company-dabbrev-other-buffers nil
 	company-dabbrev-downcase nil
 	company-require-match 'never)
   (global-set-key [remap indent-for-tab-command] #'company-indent-or-complete-common)
