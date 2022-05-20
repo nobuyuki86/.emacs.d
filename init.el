8e3291f44621954108d3942cede33fb566a01810
Kamimoto Nobuyuki <nobuyuki86@outlook.jp>
2022-05-20 22:27:41 +0900

    Add company settings

--- init.el
+++ init.el
@@ -106,12 +106,15 @@
 (straight-use-package 'company-tabnine)
 
 (with-eval-after-load 'company
-  (setq company-idle-delay 0
+  (setq company-idle-delay 0.2
 	company-minimum-prefix-length 1
 	company-dabbrev-ignore-case nil
 	company-dabbrev-other-buffers nil
 	company-dabbrev-downcase nil
-	company-require-match 'never)
+	company-require-match 'never
+	company-async-redisplay-delay 0.1
+	company-box-doc-enable nil)
+  
   (global-set-key [remap indent-for-tab-command] #'company-indent-or-complete-common)
   (global-set-key [remap c-indent-line-or-region] #'company-indent-or-complete-common)
   (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
