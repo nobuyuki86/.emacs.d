026d3cafa563f5c2e8f16126bb0f26c6ab764bea
Kamimoto Nobuyuki <nobuyuki86@outlook.jp>
2022-05-25 23:18:03 +0900

    add minad ware.
    Added marginalia.
    Added consult.
    Added embark.

--- init.el
+++ init.el
@@ -169,6 +169,149 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; #marginalia
+
+(straight-use-package 'marginalia)
+
+(marginalia-mode +1)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; #consutl
+
+(straight-use-package 'consult)
+
+;; C-c bindings (mode-specific-map)
+(global-set-key (kbd "C-c h") 'consult-history)
+(global-set-key (kbd "C-c m") 'consult-mode-command)
+(global-set-key (kbd "C-c k") 'consult-kmacro)
+;; C-x bindings (ctl-x-map)
+(global-set-key (kbd "C-x M-:") 'consult-complex-command) ;; orig. repeat-complex-command
+(global-set-key (kbd "C-x b") 'consult-buffer) ;; orig. switch-to-buffer
+(global-set-key (kbd "C-x 4 b") 'consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+(global-set-key (kbd "C-x 5 b") 'consult-buffer-other-frame) ;; orig. switch-to-buffer-other-frame
+(global-set-key (kbd "C-x r b") 'consult-bookmark) ;; orig. bookmark-jump
+(global-set-key (kbd "C-x p b") 'consult-project-buffer) ;; orig. project-switch-to-buffer
+;; Custom M-# bindings for fast register access
+(global-set-key (kbd "M-#") 'consult-register-load)
+(global-set-key (kbd "M-'") 'consult-register-store) ;; orig. abbrev-prefix-mark (unrelated)
+(global-set-key (kbd "C-M-#") 'consult-register)
+;; Other custom bindings
+(global-set-key (kbd "M-y") 'consult-yank-pop) ;; orig. yank-pop
+(global-set-key (kbd "<help> a") 'consult-apropos) ;; orig. apropos-command
+;; M-g bindings (goto-map)
+(define-key goto-map (kbd "e") 'consult-compile-error)
+(define-key goto-map (kbd "f") 'consult-flymake) ;; Alternative: consult-flycheck
+(define-key goto-map (kbd "g") 'consult-goto-line) ;; orig. goto-line
+(define-key goto-map (kbd "o") 'consult-outline) ;; Alternative: consult-org-heading
+(define-key goto-map (kbd "m") 'consult-mark)
+(define-key goto-map (kbd "k") 'consult-global-mark)
+(define-key goto-map (kbd "i") 'consult-imenu)
+(define-key goto-map (kbd "I") 'consult-imenu-multi)
+;; M-s bindings (search-map)
+(define-key search-map (kbd "d") 'consult-find)
+(define-key search-map (kbd "D") 'consult-locate)
+(define-key search-map (kbd "g") 'consult-grep)
+(define-key search-map (kbd "G") 'consult-git-grep)
+(define-key search-map (kbd "r") 'consult-ripgrep)
+(define-key search-map (kbd "l") 'consult-line)
+(define-key search-map (kbd "L") 'consult-line-multi)
+(define-key search-map (kbd "m") 'consult-multi-occur)
+(define-key search-map (kbd "k") 'consult-keep-lines)
+(define-key search-map (kbd "u") 'consult-focus-lines)
+;; Isearch integration
+(global-set-key (kbd "M-s e") 'consult-isearch-history)
+(define-key isearch-mode-map (kbd "M-e") 'consult-isearch-history) ;; orig. isearch-edit-string
+(define-key isearch-mode-map (kbd "M-s e") 'consult-isearch-history) ;; orig. isearch-edit-string
+(define-key isearch-mode-map (kbd "M-s l") 'consult-line) ;; needed by consult-line to detect isearch
+(define-key isearch-mode-map (kbd "M-s L") 'consult-line-multi) ;; needed by consult-line to detect isearch
+;; Minibuffer history
+(define-key minibuffer-local-map (kbd "M-s") 'consult-history) ;; orig. next-matching-history-element
+(define-key minibuffer-local-map (kbd "M-r") 'consult-history) ;; orig. previous-matching-history-element
+
+;; Enable automatic preview at point in the *Completions* buffer. This is
+;; relevant when you use the default completion UI.
+(add-hook 'completion-list-mode-hook 'consult-preview-at-point-mode)
+
+;; Optionally configure the register formatting. This improves the register
+;; preview for `consult-register', `consult-register-load',
+;; `consult-register-store' and the Emacs built-ins.
+(setq register-preview-delay 0.5
+      register-preview-function #'consult-register-format)
+
+;; Optionally tweak the register preview window.
+;; This adds thin lines, sorting and hides the mode line of the window.
+(advice-add #'register-preview :override #'consult-register-window)
+
+;; Use Consult to select xref locations with preview
+(setq xref-show-xrefs-function #'consult-xref
+      xref-show-definitions-function #'consult-xref)
+
+(with-eval-after-load 'consult
+  ;; Optionally configure preview. The default value
+  ;; is 'any, such that any key triggers the preview.
+  ;; (setq consult-preview-key 'any)
+  ;; (setq consult-preview-key (kbd "M-."))
+  ;; (setq consult-preview-key (list (kbd "<S-down>") (kbd "<S-up>")))
+  ;; For some commands and buffer sources it is useful to configure the
+  ;; :preview-key on a per-command basis using the `consult-customize' macro.
+  (consult-customize
+   consult-theme
+   :preview-key '(:debounce 0.2 any)
+   consult-ripgrep consult-git-grep consult-grep
+   consult-bookmark consult-recent-file consult-xref
+   consult--source-bookmark consult--source-recent-file
+   consult--source-project-recent-file
+   :preview-key (kbd "M-."))
+
+  ;; Optionally configure the narrowing key.
+  ;; Both < and C-+ work reasonably well.
+  (setq consult-narrow-key "<") ;; (kbd "C-+")
+
+  ;; Optionally make narrowing help available in the minibuffer.
+  ;; You may want to use `embark-prefix-help-command' or which-key instead.
+  ;; (define-key consult-narrow-map (vconcat consult-narrow-key "?") #'consult-narrow-help)
+
+  ;; By default `consult-project-function' uses `project-root' from project.el.
+  ;; Optionally configure a different project root function.
+  ;; There are multiple reasonable alternatives to chose from.
+  ;;;; 1. project.el (the default)
+  ;; (setq consult-project-function #'consult--default-project--function)
+  ;;;; 2. projectile.el (projectile-project-root)
+  ;; (autoload 'projectile-project-root "projectile")
+  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
+  ;;;; 3. vc.el (vc-root-dir)
+  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
+  ;;;; 4. locate-dominating-file
+  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; #emabrk
+
+(straight-use-package 'embark)
+(straight-use-package 'embark-consult)
+
+(global-set-key (kbd "C-.") 'embark-act) ;; pick some comfortable binding
+(global-set-key (kbd "C-;") 'embark-dwim) ;; good alternative: M-.
+(global-set-key (kbd "C-h B") 'embark-bindings) ;; alternative for `describe-bindings'
+
+;; Optionally replace the key help with a completing-read interface
+(setq prefix-help-command #'embark-prefix-help-command)
+
+(with-eval-after-load 'embark
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none))))
+
+  (with-eval-after-load 'consult
+    (add-hook 'embark-collect-mode 'consult-preview-at-point-mode)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; #yasnippet
 
 (straight-use-package 'yasnippet)
