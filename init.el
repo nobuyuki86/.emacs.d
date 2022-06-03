698871305224204d4ed7f927287f7594b2f0f3c9
Kamimoto Nobuyuki <nobuyuki86@outlook.jp>
2022-06-04 00:29:56 +0900

    Removed gc-cons-threshold

--- init.el
+++ init.el
@@ -35,7 +35,6 @@
   :init
   (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
 	completion-ignore-case t
-	gc-cons-threshold (* 100 1024 1024)
 	read-process-output-max (* 1024 1024)
 	use-short-answers t)
 
