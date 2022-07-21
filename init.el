f9fd260b71cee4b684e81d7a1c5ec43c8701b36d
Kamimoto Nobuyuki <nobuyuki86@outlook.jp>
2022-07-21 22:48:20 +0900

    Changed to respect the default value as much as possible

--- init.el
+++ init.el
@@ -30,59 +30,6 @@
       recentf-auto-cleanup nil
       recentf-max-saved-items 200)
 
-;; A second, case-insensitive pass over `auto-mode-alist' is time wasted, and
-;; indicates misconfiguration (don't rely on case insensitivity for file names).
-(setq auto-mode-case-fold nil)
-
-;; Disabling the BPA makes redisplay faster, but might produce incorrect display
-;; reordering of bidirectional text with embedded parentheses and other bracket
-;; characters whose 'paired-bracket' Unicode property is non-nil.
-(setq bidi-inhibit-bpa t)  ; Emacs 27 only
-
-;; Disable bidirectional text scanning for a modest performance boost. I've set
-;; this to `nil' in the past, but the `bidi-display-reordering's docs say that
-;; is an undefined state and suggest this to be just as good:
-(setq-default bidi-display-reordering 'left-to-right
-	      bidi-paragraph-direction 'left-to-right)
-
-;; Reduce rendering/line scan work for Emacs by not rendering cursors or regions
-;; in non-focused windows.
-(setq-default cursor-in-non-selected-windows nil)
-(setq highlight-nonselected-windows nil)
-
-;; More performant rapid scrolling over unfontified regions. May cause brief
-;; spells of inaccurate syntax highlighting right after scrolling, which should
-;; quickly self-correct.
-(setq fast-but-imprecise-scrolling t)
-
-;; Don't ping things that look like domain names.
-(setq ffap-machine-p-known 'reject)
-
-;; Resizing the Emacs frame can be a terribly expensive part of changing the
-;; font. By inhibiting this, we halve startup times, particularly when we use
-;; fonts that are larger than the system default (which would resize the frame).
-(setq frame-inhibit-implied-resize t)
-
-;; Emacs "updates" its ui more often than it needs to, so slow it down slightly
-(setq idle-update-delay 1.0)  ; default is 0.5
-
-;; Font compacting can be terribly expensive, especially for rendering icon
-;; fonts on Windows. Whether disabling it has a notable affect on Linux and Mac
-;; hasn't been determined, but do it there anyway, just in case. This increases
-;; memory usage, however!
-(setq inhibit-compacting-font-caches t)
-
-;; PGTK builds only: this timeout adds latency to frame operations, like
-;; `make-frame-invisible', which are frequently called without a guard because
-;; it's inexpensive in non-PGTK builds. Lowering the timeout from the default
-;; 0.1 should make childframes and packages that manipulate them (like `lsp-ui',
-;; `company-box', and `posframe') feel much snappier. See emacs-lsp/lsp-ui#613.
-(setq pgtk-wait-for-event-timeout 0.001)
-
-;; Reduce debug output, well, unless we've asked for it.
-(setq debug-on-error init-file-debug
-      jka-compr-verbose init-file-debug)
-
 (server-mode +1)
 (savehist-mode +1)
 (save-place-mode +1)
@@ -99,63 +46,6 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; emacs-ui
-
-;; The blinking cursor is distracting, but also interferes with cursor settings
-;; in some minor modes that try to change it buffer-locally (like treemacs) and
-;; can cause freezing for folks (esp on macOS) with customized & color cursors.
-(blink-cursor-mode -1)
-
-;; Don't blink the paren matching the one at point, it's too distracting.
-(setq blink-matching-paren nil)
-
-;; Reduce the clutter in the fringes; we'd like to reserve that space for more
-;; useful information, like git-gutter and flycheck.
-(setq indicate-buffer-boundaries nil
-      indicate-empty-lines nil)
-
-;; Don't resize the frames in steps; it looks weird, especially in tiling window
-;; managers, where it can leave unseemly gaps.
-(setq frame-resize-pixelwise t)
-
-;; But do not resize windows pixelwise, this can cause crashes in some cases
-;; when resizing too many windows at once or rapidly.
-(setq window-resize-pixelwise nil)
-
-;; The native border "consumes" a pixel of the fringe on righter-most splits,
-;; `window-divider' does not. Available since Emacs 25.1.
-(setq window-divider-default-places t
-      window-divider-default-bottom-width 1
-      window-divider-default-right-width 1)
-(window-divider-mode +1)
-
-;; GUIs are inconsistent across systems and themes (and will rarely match our
-;; active Emacs theme). They impose inconsistent shortcut key paradigms too.
-;; It's best to avoid them altogether and have Emacs handle the prompting.
-(setq use-dialog-box nil)
-(when (bound-and-true-p tooltip-mode)
-  (tooltip-mode -1))
-(when (eq system-type 'gnu/linux)
-  (setq x-gtk-use-system-tooltips nil))
-
-;; Allow for minibuffer-ception. Sometimes we need another minibuffer command
-;; while we're in the minibuffer.
-(setq enable-recursive-minibuffers t)
-
-;; Show current key-sequence in minibuffer ala 'set showcmd' in vim. Any
-;; feedback after typing is better UX than no feedback at all.
-(setq echo-keystrokes 0.02)
-
-;; Expand the minibuffer to fit multi-line text displayed in the echo-area. This
-;; doesn't look too great with direnv, however...
-(setq resize-mini-windows 'grow-only)
-
-;; Try to keep the cursor out of the read-only portions of the minibuffer.
-(setq minibuffer-prompt-properties '(read-only t intangible t cursor-intangible t face minibuffer-prompt))
-(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; #character-code
 
 (set-language-environment "Japanese")
@@ -166,13 +56,6 @@
   (set-keyboard-coding-system 'cp932)
   (set-terminal-coding-system 'cp932)
 
-  (set-charset-priority 'ascii
-			'japanese-jisx0208
-			'latin-jisx0201
-			'katakana-jisx0201
-			'iso-8859-1
-			'cp1252
-			'unicode)
   (set-coding-system-priority 'utf-8
 			      'euc-jp
 			      'iso-2022-jp
@@ -233,18 +116,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; #modeline
 
-(straight-use-package 'moody)
-(straight-use-package 'minions)
+(straight-use-package 'doom-modeline)
 (straight-use-package 'nyan-mode)
 
-(setq x-underline-at-descent-line t
-      nyan-animate-nyancat t
+(setq nyan-animate-nyancat t
       nyan-bar-length 24)
 
-(moody-replace-mode-line-buffer-identification)
-(moody-replace-vc-mode)
-(moody-replace-eldoc-minibuffer-message-function)
-(minions-mode +1)
+(doom-modeline-mode +1)
 (nyan-mode +1)
 
 
@@ -312,7 +190,8 @@
 (straight-use-package 'evil-lion)
 
 (setq evil-want-keybinding nil
-      evil-symbol-word-search t)
+      evil-symbol-word-search t
+      evil-kill-on-visual-paste nil)
 
 (define-key my-buffer-map (kbd "d") #'evil-delete-buffer)
 
@@ -368,19 +247,15 @@
 (straight-use-package '(company-dwim :type git :host github :repo "zk-phi/company-dwim"))
 (straight-use-package '(company-anywhere :type git :host github :repo "zk-phi/company-anywhere"))
 
-(setq company-idle-delay 0
-      company-minimum-prefix-length 1
-      company-tooltip-align-annotations t
-      company-dabbrev-other-buffers nil
-      company-dabbrev-ignore-case nil
-      company-dabbrev-downcase nil
-      company-require-match 'never
-      company-auto-complete nil)
+(setq company-minimum-prefix-length 1)
 
 (with-eval-after-load 'company
-  (add-to-list 'company-backends '(company-capf company-yasnippet company-tabnine :separate))
-
   (require 'company-dwim)
+  (require 'company-anywhere)
+
+  (add-to-list 'company-backends '(company-capf company-yasnippet company-tabnine :separate))
+  (add-to-list 'company-frontends 'company-dwim-frontend t)
+
   (define-key company-active-map (kbd "RET") nil)
   (define-key company-active-map (kbd "<return>") nil)
   (define-key company-active-map (kbd "TAB") #'company-dwim)
@@ -388,10 +263,7 @@
   (define-key company-active-map (kbd "S-TAB") #'company-dwim-select-previous)
   (define-key company-active-map (kbd "<backtab>") #'company-dwim-select-previous)
   (define-key company-active-map (kbd "C-TAB") #'company-complete-selection)
-  (define-key company-active-map (kbd "C-<tab>") #'company-complete-selection)
-  (add-to-list 'company-frontends 'company-dwim-frontend t)
-
-  (require 'company-anywhere))
+  (define-key company-active-map (kbd "C-<tab>") #'company-complete-selection))
 
 (global-set-key [remap indent-for-tab-command] #'company-indent-or-complete-common)
 (global-set-key [remap c-indent-line-or-region] #'company-indent-or-complete-common)
@@ -408,10 +280,9 @@
 
 (global-set-key (kbd "C-x C-z") #'selectrum-repeat)
 
-(with-eval-after-load 'selectrum
-  (with-eval-after-load 'evil
-    (evil-define-key '(normal visual) my-intercept-mode-map
-      (kbd "SPC z") 'selectrum-repeat)))
+(with-eval-after-load 'evil
+  (evil-define-key '(normal visual) my-intercept-mode-map
+    (kbd "SPC z") 'selectrum-repeat))
 
 (selectrum-mode +1)
 
@@ -429,7 +300,6 @@
   (prescient-persist-mode +1))
 
 (selectrum-prescient-mode +1)
-
 (company-prescient-mode +1)
 
 
@@ -592,11 +462,12 @@
 (straight-use-package 'yasnippet-snippets)
 
 (with-eval-after-load 'yasnippet
+  (require 'yasnippet-snippets)
+
   (define-key yas-minor-mode-map (kbd "TAB") nil)
   (define-key yas-minor-mode-map (kbd "<tab>") nil)
   (define-key yas-keymap (kbd "C-TAB") 'yas-next-field-or-maybe-expand)
-  (define-key yas-keymap (kbd "C-<tab>") 'yas-next-field-or-maybe-expand)
-  (require 'yasnippet-snippets))
+  (define-key yas-keymap (kbd "C-<tab>") 'yas-next-field-or-maybe-expand))
 
 (yas-global-mode +1)
 
@@ -645,23 +516,18 @@
 (define-key my-error-map (kbd "p") #'flycheck-previous-error)
 (define-key my-error-map (kbd "e") #'consult-flycheck)
 
+(add-hook 'flycheck-mode-hook #'flycheck-posframe-mode)
+
 (with-eval-after-load 'flycheck
-  ;; Rerunning checks on every newline is a mote excessive.
-  (delq 'new-line flycheck-check-syntax-automatically)
-  ;; And don't recheck on idle as often
-  (setq flycheck-idle-change-delay 1.0)
-  ;; For the above functionality, check syntax in a buffer that you switched to
-  ;; only briefly. This allows "refreshing" the syntax check state for several
-  ;; buffers quickly after e.g. changing a config file.
-  (setq flycheck-buffer-switch-check-intermediate-buffers t)
-  ;; Display errors a little quicker (default is 0.9s)
-  (setq flycheck-display-errors-delay 0.25)
-
   (require 'flycheck-posframe)
-  (add-hook 'flycheck-mode-hook #'flycheck-posframe-mode)
+  ;; (delq 'new-line flycheck-check-syntax-automatically)
+  ;; (setq flycheck-idle-change-delay 1.0
+  ;; 	flycheck-buffer-switch-check-intermediate-buffers t
+  ;; 	flycheck-display-errors-delay 0.25)
+  )
 
-  (with-eval-after-load 'company
-    (add-hook 'flycheck-posframe-inhibit-functions #'company--active-p)))
+(with-eval-after-load 'company
+  (add-hook 'flycheck-posframe-inhibit-functions #'company--active-p))
 
 (global-flycheck-mode +1)
 
@@ -733,21 +599,17 @@
 		:base05 "#2f2725" ;; 墨色
 		:base06 "#405C36" ;; 老緑
 		:base07 "#EEBBCB" ;; 撫子色
-		:base08 "#E9546B" ;; 梅重
+		:base08 "#EA0032" ;; 唐紅
 		:base09 "#C92E36" ;; 柘榴色
 		:base0A "#0086AD" ;; 花色
 		:base0B "#7BAA17" ;; 柳緑
 		:base0C "#22825D" ;; 木賊色
-		:base0D "#E73275" ;; 薔薇色
+		:base0D "#E2421F" ;; 紅葉色
 		:base0E "#6967AB" ;; 竜胆色
 		:base0F "#6A1435" ;; 紫檀色
 		))
 
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(highlight ((t (:background "#FEEEED" :underline t)))))
 
 (load-theme 'base16-sakura t)
@@ -772,7 +634,6 @@
 
 (when (display-graphic-p)
   (require 'all-the-icons)
-
   (add-hook 'dired-mode-hook 'all-the-icons-dired-mode))
 
 
@@ -809,10 +670,6 @@
 ;; #gcmh
 
 (straight-use-package 'gcmh)
-
-(setq gcmh-idle-delay 'auto
-      gcmh-auto-idle-delay-factor 10
-      gcmh-high-cons-threshold (* 128 1024 1024))
 
 (gcmh-mode +1)
 
@@ -991,9 +848,11 @@
 
 (straight-use-package 'tree-sitter)
 (straight-use-package 'tree-sitter-langs)
+(straight-use-package '(ts-fold :type git :host github :repo "jcs-elpa/ts-fold"))
 
 (require 'tree-sitter)
 (require 'tree-sitter-langs)
+(require 'ts-fold)
 
 (global-tree-sitter-mode)
 
@@ -1017,7 +876,7 @@
 			   (with-eval-after-load 'evil
 			     (evil-local-set-key 'normal (kbd "SPC m") `("lsp" . ,lsp-command-map)))))
 
-(add-hook 'web-mode-hook #'lsp)
+(add-hook 'html-mode-hook #'lsp)
 (add-hook 'css-mode-hook #'lsp)
 (add-hook 'rust-mode-hook #'lsp)
 (add-hook 'java-mode-hook (lambda ()
@@ -1037,19 +896,6 @@
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; #sidekick
-
-(straight-use-package '(sidekick :type git :host github :repo "VernonGrant/sidekick.el"))
-
-(require 'sidekick)
-(define-key search-map (kbd "k") #'sidekick-at-point)
-(define-key search-map (kbd "K") #'sidekick-search-for-literal)
-
-(with-eval-after-load 'sidekick
-  (require 'sidekick-evil))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; #java
 
 (add-hook 'java-mode-hook (lambda ()
@@ -1061,16 +907,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; #web
 
-(straight-use-package 'web-mode)
 (straight-use-package 'emmet-mode)
 (straight-use-package 'web-beautify)
 
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . html-mode))
 
-(add-hook 'web-mode-hook (lambda ()
-			   (setq-local tab-width 2)
-			   (emmet-mode +1)))
+(add-hook 'html-mode-hook (lambda ()
+			    (setq-local tab-width 2)
+			    (emmet-mode +1)))
 
 (add-hook 'css-mode-hook (lambda ()
 			   (setq-local tab-width 2)
