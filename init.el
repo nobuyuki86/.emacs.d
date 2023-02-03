5529f29ce93e41cf66c8b6a97af00f5ee869c15c
Nobuyuki Kamimoto <nobuyuki86@outlook.jp>
2023-02-04 00:33:56 +0900

    フォントの調整

--- init.el
+++ init.el
@@ -59,6 +59,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil)
  '(warning-suppress-types '((emacs) (org-element-cache) (initialization) (comp))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
